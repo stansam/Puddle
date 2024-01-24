d681129baa83c0f98c4ca14962fb4394261c6d13
Karume-lab <danielkarume@outlook.com>
2024-01-24 19:18:27 +0300

    configured allauth urls

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("accounts/", include("apps.accounts.urls")),
 ]
