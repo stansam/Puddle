ec838410ddc35610ec4d74f4dde37c0da8321da8
Javan Odhiambo <114527546+Javan-Odhiambo@users.noreply.github.com>
2024-01-25 17:57:23 +0000

    changed static root path

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
